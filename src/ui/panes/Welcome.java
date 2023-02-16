9359f7d49fcfe424de1d0707e23c80ba9c17df8d
Ricardo Soriano <ricardo.roel110@gmail.com>
2023-02-16 17:14:54 -0600

    Commented previous commit

--- src/ui/panes/Welcome.java
+++ src/ui/panes/Welcome.java
@@ -5,20 +5,25 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class Welcome extends JFrame {
+    // Here are the items that will be showed on the main page
     private JPanel mainPanel;
     private JRadioButton distanciaRadioButton;
     private JRadioButton fuerzaRadioButton;
     private JRadioButton cargaRadioButton;
     private JButton nextButton;
 
+    // This int is going to change its value in each radio button case
     private int selectedRadio;
 
     public Welcome () {
+        // This will show the main panel, that shows everything
         setContentPane(mainPanel);
-        setSize(700, 700);
+        setSize(300, 300);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
+        setLocationRelativeTo(null);
 
+        // This will get the selected radio button and print the index
         nextButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -26,6 +31,7 @@ public class Welcome extends JFrame {
             }
         });
 
+        // If distanciaRadioButton is clicked, will change the selectedRadio to 0
         distanciaRadioButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -33,6 +39,7 @@ public class Welcome extends JFrame {
             }
         });
 
+        // If fuerzaRadioButton is clicked, will change the selectedRadio to 1
         fuerzaRadioButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -40,6 +47,7 @@ public class Welcome extends JFrame {
             }
         });
 
+        // If cargaRadioButton is clicked, will change the selectedRadio to 2
         cargaRadioButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -48,6 +56,7 @@ public class Welcome extends JFrame {
         });
     }
 
+    // this will initialize the page
     public static void main() {
         Welcome welcome = new Welcome();
     }
