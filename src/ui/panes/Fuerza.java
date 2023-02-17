f15342dfaf1e8d0c1b3e67f0d00fe77e91898f53
Ricardo Soriano <ricardo.roel110@gmail.com>
2023-02-16 18:15:28 -0600

    changed Carga, Distancia and Fuerza UI

--- src/ui/panes/Fuerza.java
+++ src/ui/panes/Fuerza.java
@@ -1,9 +1,13 @@
 package ui.panes;
 
 import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Fuerza extends JFrame{
     private JPanel mainPanel;
+    private JButton returnButton;
+    private JButton calculateButton;
 
     public Fuerza () {
         // This will show the main panel, that shows everything
@@ -12,5 +16,13 @@ public class Fuerza extends JFrame{
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
         setLocationRelativeTo(null);
+
+        returnButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                Welcome welcome = new Welcome();
+                setVisible(false);
+            }
+        });
     }
 }
